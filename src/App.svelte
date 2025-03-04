c41f8c26a67063dc21998a4ad7e61a16e664dca3
Habsyi2512 <githabsyi@gmail.com>
2025-03-04 19:18:13 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import FormInput from "./components/FormInput.svelte";
+
+  let todos: { todo: string; isDone: boolean }[] = $state([]);
+</script>
+
+<main class="bg-[url('/background.jpg')] min-h-screen pt-[30px]">
+  <div
+    class="bg-white/40 border border-neutral-300 shadow-lg shadow-neutral-800/20 backdrop-blur-2xl w-full max-w-3xl rounded-lg mx-auto"
+  >
+    <h1 class="text-3xl font-bold text-center py-5 text-neutral-600">
+      Todo APP
+    </h1>
+
+    {#each todos as todo}
+      <li>{todo.todo}</li>
+    {:else}
+      <div>Tidak Ada todo</div>
+    {/each}
+  </div>
+
+  <FormInput {todos} />
+</main>
+
+<style></style>
---
+++ src/components/FormInput.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import PaperAirplanes from "./icons/PaperAirplanes.svelte";
+
+  let { todos = $bindable([]) } = $props();
+
+  let text = $state("");
+  const maxHeight = 150;
+
+  function autoResize(event: Event) {
+    const textarea = event.target as HTMLTextAreaElement;
+
+    textarea.style.height = "auto";
+
+    const newHeight = Math.min(textarea.scrollHeight, maxHeight);
+    textarea.style.height = `${newHeight}px`;
+  }
+
+  function handleSubmit(event: Event) {
+    event.preventDefault();
+    if (text.trim()) {
+      todos = [...todos, { todo: text, isDone: false }];
+      text = "";
+    }
+  }
+
+  $effect(() => {
+    $inspect("todos = ", todos);
+  });
+</script>
+
+<form
+  onsubmit={handleSubmit}
+  class="bg-white/40 border px-5 border-neutral-300 shadow-lg shadow-neutral-800/20 backdrop-blur-2xl w-full max-w-3xl rounded-lg p-3 mx-auto fixed left-1/2 -translate-x-1/2 bottom-10"
+>
+  <div class="flex items-end gap-x-5">
+    <textarea
+      bind:value={text}
+      oninput={(event) => {
+        autoResize(event);
+      }}
+      rows="1"
+      class="w-full resize-none focus:outline-0 overflow-y-auto transition-all"
+      placeholder="What needs to be done?"
+    ></textarea>
+    <button aria-label="Submit todo">
+      <PaperAirplanes className="w-7 h-7 text-neutral-600" />
+    </button>
+  </div>
+</form>
+
+<style>
+  /* your styles go here */
+</style>
